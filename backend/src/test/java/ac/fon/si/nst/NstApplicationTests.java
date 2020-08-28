495e9cb369a6a6912b6b3f6d9b8320769db3a7a2
kjerinic <katarinajerinic8@gmail.com>
2020-08-28 17:06:31 +0200

    Initialize empty Spring Boot project using Spring Initializr

---
+++ backend/src/test/java/ac/fon/si/nst/NstApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.fon.si.nst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
